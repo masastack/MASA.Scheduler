5c1eaec474c4d997105b57bb0822efe8d9a8c925
Gavin Li <54987555+gavin1ee@users.noreply.github.com>
2022-07-05 09:22:25 +0800

    fix: add test get oss secret all key API (#20)

--- src/Services/Masa.Scheduler.Services.Server/Services/OssService.cs
+++ src/Services/Masa.Scheduler.Services.Server/Services/OssService.cs
@@ -8,6 +8,7 @@ public class OssService : ServiceBase
     public OssService(IServiceCollection services) : base(services, ConstStrings.OSS_API)
     {
         MapGet(GetSecurityTokenAsync);
+        MapGet(TestSecretStore);
     }
 
     private async Task<GetSecurityTokenDto> GetSecurityTokenAsync([FromServices] IClient client, [FromServices] DaprClient daprClient)
@@ -19,5 +20,24 @@ public class OssService : ServiceBase
         var accessSecret = response.AccessKeySecret;
         var bucket = daprClient.GetSecretAsync("localsecretstore", "bucket").Result.First().Value;
         return await Task.FromResult(new GetSecurityTokenDto(region, accessId, accessSecret, stsToken, bucket));
+    }
+
+    private async Task<IResult> TestSecretStore(string storeName, [FromServices] DaprClient daprClient)
+    {
+        var result = await daprClient.GetBulkSecretAsync(storeName);
+        foreach (var resultKey in result.Keys)
+        {
+            Console.WriteLine("*****************************");
+            Console.WriteLine($"Result Key: {resultKey}");
+            var value = result[resultKey];
+            foreach (var key in value.Keys)
+            {
+                Console.WriteLine("------------------------------------");
+                Console.WriteLine($"loadKey: {key}, Value: {value[key]}");
+                Console.WriteLine("------------------------------------");
+            }
+            Console.WriteLine("*****************************");
+        }
+        return Results.Ok();
     }
 }
