891c93e6b60a5588b790ef79de45b4d9bfbd0b0e
Gavin <kevin_1ee@163.com>
2022-07-18 11:32:18 +0800

    fix: local datetime

---
+++ src/Web/Masa.Scheduler.Web.Admin/Pages/Teams/Components/SubComponents/DateTimeFormat.razor.cs
@@ -0,0 +1,29 @@
+﻿// Copyright (c) MASA Stack All rights reserved.
+// Licensed under the Apache License. See LICENSE.txt in the project root for license information.
+
+namespace Masa.Scheduler.Web.Admin.Pages.Teams.Components.SubComponents;
+
+public partial class DateTimeFormat
+{
+    [Parameter]
+    public DateTimeOffset Value { get; set; }
+
+    [Parameter]
+    public string Format { get; set; } = "yyyy-MM-dd HH:mm:ss";
+
+    private string _valueFormatString = string.Empty;
+
+    protected override async Task OnInitializedAsync()
+    {
+        if(Value != DateTimeOffset.MinValue)
+        {
+            var localTimeStr = await JsInvokeAsync<string>("toLocalTime", Value);
+
+            var localDateTime = Convert.ToDateTime(localTimeStr);
+
+            _valueFormatString = localDateTime.ToString(Format);
+        }
+
+        await base.OnInitializedAsync();
+    }
+}
