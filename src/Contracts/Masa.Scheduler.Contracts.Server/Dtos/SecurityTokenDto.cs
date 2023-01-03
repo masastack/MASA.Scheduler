f0cb4354d523689a115ee7c973763d16e717eb5a
wzh425 <wuzihao425@outlook.com>
2023-01-03 19:50:03 +0800

    fix : Folder case (#132)
    * style :  rename SecurityTokenDto
    
    * chore : remove GetSecurityTokenDto
    
    * chore : add SecurityTokenDto
    
    * chore : rename
    
    * chore : rename

---
+++ src/Contracts/Masa.Scheduler.Contracts.Server/Dtos/SecurityTokenDto.cs
@@ -0,0 +1,26 @@
+﻿// Copyright (c) MASA Stack All rights reserved.
+// Licensed under the Apache License. See LICENSE.txt in the project root for license information.
+
+namespace Masa.Scheduler.Contracts.Server.Dtos;
+
+public class SecurityTokenDto
+{
+    public string Region { get; set; }
+
+    public string AccessKeyId { get; set; }
+
+    public string AccessKeySecret { get; set; }
+
+    public string StsToken { get; set; }
+
+    public string Bucket { get; set; }
+
+    public SecurityTokenDto(string region, string accessKeyId, string accessKeySecret, string stsToken, string bucket)
+    {
+        Region = region;
+        AccessKeyId = accessKeyId;
+        AccessKeySecret = accessKeySecret;
+        StsToken = stsToken;
+        Bucket = bucket;
+    }
+}
