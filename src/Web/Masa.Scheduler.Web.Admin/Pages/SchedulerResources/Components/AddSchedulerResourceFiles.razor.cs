688990fc679e9b0d3a01380895a10456a4545d1b
wzh425 <wuzihao425@outlook.com>
2023-03-02 15:31:23 +0800

    style : global using

--- src/Web/Masa.Scheduler.Web.Admin/Pages/SchedulerResources/Components/AddSchedulerResourceFiles.razor.cs
+++ src/Web/Masa.Scheduler.Web.Admin/Pages/SchedulerResources/Components/AddSchedulerResourceFiles.razor.cs
@@ -1,8 +1,6 @@
 ﻿// Copyright (c) MASA Stack All rights reserved.
 // Licensed under the Apache License. See LICENSE.txt in the project root for license information.
 
-using Masa.Blazor.Presets;
-
 namespace Masa.Scheduler.Web.Admin.Pages.SchedulerResources.Components;
 
 public partial class AddSchedulerResourceFiles
