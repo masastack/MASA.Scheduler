664962f1fe34a93eb8a58b4dfb46f655f59c7434
LiMing <38368335+MapleWithoutWords@users.noreply.github.com>
2023-03-03 13:36:41 +0800

    fix:The file name is changed after the file is downloaded. resolve #138 (#219)

---
+++ src/Web/Masa.Scheduler.Web.Admin/wwwroot/js/download-file.js
@@ -0,0 +1,27 @@
+﻿window._blazorDownloadFile=function(url, filename) {
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
+    xhr.responseType = 'blob';
+    xhr.onload = () => {
+        if (xhr.status === 200) {
+            var blob = xhr.response;
+            if (window.navigator.msSaveOrOpenBlob) {
+                navigator.msSaveBlob(blob, filename);
+            } else {
+                const link = document.createElement('a');
+                const body = document.querySelector('body');
+
+                link.href = window.URL.createObjectURL(blob);
+                link.download = filename;
+                link.style.display = 'none';
+                body.appendChild(link);
+
+                link.click();
+                body.removeChild(link);
+
+                window.URL.revokeObjectURL(link.href);
+            }
+        }
+    };
+    xhr.send();
+}
