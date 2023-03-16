264878a2b3e8cd7150ddc614036a56322be6000e
yazhouzhang <y_az@163.com>
2023-03-16 15:47:14 +0800

    using move to global using, global using sorting. Fix datetimerangepi… (#249)
    * using move to global using, global using sorting. Fix datetimerangepicker style modify, datetime displayed in one line. Fix Resource Tab item text be case sensitive. Fixed MCard expansion and collapse function and icon
    
    * The query time type excludes the creation time and modification time
    
    * Remove empty line

--- src/Web/Masa.Scheduler.Web.Admin/Pages/SchedulerResources/SchedulerResourceFiles.razor.cs
+++ src/Web/Masa.Scheduler.Web.Admin/Pages/SchedulerResources/SchedulerResourceFiles.razor.cs
@@ -1,8 +1,6 @@
 ﻿// Copyright (c) MASA Stack All rights reserved.
 // Licensed under the Apache License. See LICENSE.txt in the project root for license information.
 
-using Masa.Scheduler.Web.Admin.Pages.SchedulerResources.Components;
-
 namespace Masa.Scheduler.Web.Admin.Pages.SchedulerResources;
 
 public partial class SchedulerResourceFiles
