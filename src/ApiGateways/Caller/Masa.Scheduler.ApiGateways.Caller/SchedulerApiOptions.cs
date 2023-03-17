528bbcf28bc82e93b52cdaac83e1eb86714f4b4e
Li Ming <1028139084@qq.com>
2023-03-17 16:49:51 +0800

    fix:After the drop-down box is opened, slide the mouse to keep the expanded drop-down box in a fixed position,resolved #254

--- src/ApiGateways/Caller/Masa.Scheduler.ApiGateways.Caller/SchedulerApiOptions.cs
+++ src/ApiGateways/Caller/Masa.Scheduler.ApiGateways.Caller/SchedulerApiOptions.cs
@@ -6,4 +6,10 @@ namespace Masa.Scheduler.ApiGateways.Caller;
 public class SchedulerApiOptions
 {
     public string SchedulerServerBaseAddress { get; set; } = string.Empty;
+
+    public string AuthorityEndpoint { get; set; } = string.Empty;
+
+    public string ClientId { get; set; } = string.Empty;
+
+    public string ClientSecret { get; set; } = string.Empty;
 }
