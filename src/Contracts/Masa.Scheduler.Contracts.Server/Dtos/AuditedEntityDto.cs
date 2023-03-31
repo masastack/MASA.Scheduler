8eaa958d7570a2e540b0d9d3018e7170886a6405
wzh425 <wuzihao425@outlook.com>
2023-03-31 17:01:49 +0800

    fix : Clean up issues (#273)

---
+++ src/Contracts/Masa.Scheduler.Contracts.Server/Dtos/AuditedEntityDto.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) MASA Stack All rights reserved.
+// Licensed under the Apache License. See LICENSE.txt in the project root for license information.
+
+namespace Masa.Scheduler.Contracts.Server.Dtos;
+
+public class AuditedEntityDto
+{
+    public DateTime CreationTime { get; set; }
+
+    public Guid Creator { get; set; }    
+
+    public string CreatorName { get; set; } = string.Empty;
+
+    public DateTime ModificationTime { get; set; }
+
+    public Guid Modifier { get; set; }
+
+    public string ModifierName { get; set; } = string.Empty;
+}
