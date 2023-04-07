800249c7e4ecefa74686fb930b28155801dc412e
wzh425 <wuzihao425@outlook.com>
2023-04-07 17:10:24 +0800

    chore : Remove debugging code

--- src/Web/Masa.Scheduler.Web.Admin.Server/Program.cs
+++ src/Web/Masa.Scheduler.Web.Admin.Server/Program.cs
@@ -47,7 +47,7 @@ schedulerBaseAddress = "https://localhost:19611";
 var signalRBaseAddress = schedulerBaseAddress + "/server-hub/notifications";
 
 builder.AddMasaStackComponentsForServer("wwwroot/i18n", authBaseAddress, mcBaseAddress);
-builder.Services.AddTscClient(masaStackConfig.GetTscServiceDomain()).AddAlertClient("https://localhost:19701");
+builder.Services.AddTscClient(masaStackConfig.GetTscServiceDomain()).AddAlertClient(masaStackConfig.GetAlertServiceDomain());
 builder.Services.AddHttpContextAccessor();
 
 builder.Services.AddMapster();
