f72805a8e99664b7720c20ba26915dd48fa9927d
wzh425 <wuzihao425@outlook.com>
2023-04-13 13:43:01 +0800

    chore : Remove debug code

--- src/Services/Masa.Scheduler.Services.Server/Program.cs
+++ src/Services/Masa.Scheduler.Services.Server/Program.cs
@@ -30,8 +30,8 @@ builder.Services.AddObservable(builder.Logging, () =>
 {
     return masaStackConfig.OtlpUrl;
 });
-var connectString = "Server=.;Database=scheduler_dev;User Id=sa;Password=Tcsnwzh425;";
-var quartzConnectString = connectString;
+
+var quartzConnectString = masaStackConfig.GetConnectionString(AppSettings.Get("DBName"));
 var publicConfiguration = builder.Services.GetMasaConfiguration().ConfigurationApi.GetPublic();
 var identityServerUrl = masaStackConfig.GetSsoDomain();
 
@@ -134,7 +134,7 @@ builder.Services
         {
             eventBusBuilder.UseMiddleware(typeof(ValidatorMiddleware<>));
         })
-        .UseUoW<SchedulerDbContext>(dbOptions => dbOptions.UseSqlServer(connectString).UseFilter())
+        .UseUoW<SchedulerDbContext>(dbOptions => dbOptions.UseSqlServer(masaStackConfig.GetConnectionString(AppSettings.Get("DBName"))).UseFilter())
         .UseRepository<SchedulerDbContext>();
     }).AddIsolation(isolationBuilder => isolationBuilder.UseMultiEnvironment(IsolationConsts.ENVIRONMENT));
 
