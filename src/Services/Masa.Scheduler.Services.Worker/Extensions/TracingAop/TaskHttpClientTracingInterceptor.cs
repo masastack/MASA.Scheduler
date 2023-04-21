f2f4edb67185f5ab649c6281673111e666fac77b
Li Ming <1028139084@qq.com>
2023-04-21 14:21:55 +0800

    delete comment code

--- src/Services/Masa.Scheduler.Services.Worker/Extensions/TracingAop/TaskHttpClientTracingInterceptor.cs
+++ src/Services/Masa.Scheduler.Services.Worker/Extensions/TracingAop/TaskHttpClientTracingInterceptor.cs
@@ -7,16 +7,6 @@ public class TaskHttpClientTracingInterceptor : HttpClientTracingInterceptorBase
 {
     public override void OnHttpResponseMessage(Activity activity, HttpResponseMessage responseMessage)
     {
-        //var queryString = responseMessage.RequestMessage!.RequestUri!.Query.Trim('?');
-        //if (queryString.Contains("taskId"))
-        //{
-        //    var taskIdQuery = queryString.Split("&").FirstOrDefault(e => e.Contains("taskId"));
-        //    if (taskIdQuery != null && Guid.TryParse(taskIdQuery.Split('=')[1], out Guid taskId))
-        //    {
-        //        var eventBus = ServiceProvider!.GetRequiredService<IEventBus>();
-        //        eventBus.PublishAsync(new SetHttpTaskTracingIntegrationEvent { TaskId = taskId, TraceId = activity?.TraceId.ToString() }).ConfigureAwait(false).GetAwaiter();
-        //    }
-        //}
     }
 
     public override void OnHttpRequestMessage(Activity activity, HttpRequestMessage requestMessage)
