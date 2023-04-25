b56fffa3a7e049c1a0fb84d20b6f91827cd4c776
MayueCif <6133287+MayueCif@users.noreply.github.com>
2023-04-25 20:02:34 +0800

    fix:certificate (#414)

--- src/Services/Masa.Scheduler.Services.Server/Program.cs
+++ src/Services/Masa.Scheduler.Services.Server/Program.cs
@@ -1,5 +1,5 @@
-﻿// Copyright (c) MASA Stack All rights reserved.
-// Licensed under the Apache License. See LICENSE.txt in the project root for license information.
+﻿// Copyright (c) MASA Stack All rights reserved.
+// Licensed under the Apache License. See LICENSE.txt in the project root for license information.
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -67,6 +67,15 @@ builder.Services
     options.RequireHttpsMetadata = false;
     options.TokenValidationParameters.ValidateAudience = false;
     options.MapInboundClaims = false;
+    options.BackchannelHttpHandler = new HttpClientHandler
+    {
+        ServerCertificateCustomValidationCallback = (
+            sender,
+            certificate,
+            chain,
+            sslPolicyErrors) =>
+        { return true; }
+    };
 });
 
 var redisOptions = new RedisConfigurationOptions
