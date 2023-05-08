49ae879d6a7a740c85bb23ce204c0180db438196
wzh425 <wuzihao425@outlook.com>
2023-05-08 10:11:12 +0800

    fix : Message exceeds the field length limit

---
+++ src/Services/Masa.Scheduler.Services.Server/Migrations/20230507133253_TaskMessage.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Masa.Scheduler.Services.Server.Migrations
+{
+    public partial class TaskMessage : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Message",
+                schema: "server",
+                table: "SchedulerTask",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(255)",
+                oldMaxLength: 255);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Message",
+                schema: "server",
+                table: "SchedulerTask",
+                type: "nvarchar(255)",
+                maxLength: 255,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+    }
+}
