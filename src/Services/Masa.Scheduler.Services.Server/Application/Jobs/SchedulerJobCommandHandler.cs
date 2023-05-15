0af70588a227cc99a0c1deec470bc9ba708af043
wzh425 <wuzihao425@outlook.com>
2023-05-15 11:19:58 +0800

    style :  global using

--- src/Services/Masa.Scheduler.Services.Server/Application/Jobs/SchedulerJobCommandHandler.cs
+++ src/Services/Masa.Scheduler.Services.Server/Application/Jobs/SchedulerJobCommandHandler.cs
@@ -1,8 +1,6 @@
 ﻿// Copyright (c) MASA Stack All rights reserved.
 // Licensed under the Apache License. See LICENSE.txt in the project root for license information.
 
-using static Google.Rpc.Context.AttributeContext.Types;
-
 namespace Masa.Scheduler.Services.Server.Application.Jobs;
 
 public class SchedulerJobCommandHandler
