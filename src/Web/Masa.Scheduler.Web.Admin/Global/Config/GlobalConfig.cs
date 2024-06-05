bead49de37f8689e4d22cd259705d0303c514434
wzh425 <wuzihao425@outlook.com>
2024-06-05 13:50:51 +0800

    refactor: Use user-select-item template (#490)

--- src/Web/Masa.Scheduler.Web.Admin/Global/Config/GlobalConfig.cs
+++ src/Web/Masa.Scheduler.Web.Admin/Global/Config/GlobalConfig.cs
@@ -7,67 +7,11 @@ public class GlobalConfig
 {
     #region Field
 
-    private bool _isDark;
-    private string? _pageMode;
-    private bool _expandOnHover;
-    private bool _navigationMini;
-    private string? _favorite;
-    private CookieStorage? _cookieStorage;
     private bool _loading;
 
     #endregion
 
     #region Property
-
-    public static string IsDarkCookieKey { get; set; } = "GlobalConfig_IsDark";
-
-    public static string PageModeKey { get; set; } = "GlobalConfig_PageMode";
-
-    public static string NavigationMiniCookieKey { get; set; } = "GlobalConfig_NavigationMini";
-
-    public static string ExpandOnHoverCookieKey { get; set; } = "GlobalConfig_ExpandOnHover";
-
-    public static string FavoriteCookieKey { get; set; } = "GlobalConfig_Favorite";
-
-    public bool IsDark
-    {
-        get => _isDark;
-        set
-        {
-            _isDark = value;
-            _cookieStorage?.SetItemAsync(IsDarkCookieKey, value);
-        }
-    }
-
-    public bool NavigationMini
-    {
-        get => _navigationMini;
-        set
-        {
-            _navigationMini = value;
-            _cookieStorage?.SetItemAsync(NavigationMiniCookieKey, value);
-        }
-    }
-
-    public bool ExpandOnHover
-    {
-        get => _expandOnHover;
-        set
-        {
-            _expandOnHover = value;
-            _cookieStorage?.SetItemAsync(ExpandOnHoverCookieKey, value);
-        }
-    }
-
-    public string? Favorite
-    {
-        get => _favorite;
-        set
-        {
-            _favorite = value;
-            _cookieStorage?.SetItemAsync(FavoriteCookieKey, value);
-        }
-    }
 
     public bool Loading
     {
@@ -86,12 +30,6 @@ public class GlobalConfig
 
     #endregion
 
-    public GlobalConfig(CookieStorage cookieStorage, IHttpContextAccessor httpContextAccessor)
-    {
-        _cookieStorage = cookieStorage;
-        if (httpContextAccessor.HttpContext is not null) Initialization(httpContextAccessor.HttpContext.Request.Cookies);
-    }
-
     #region event
 
     public delegate void GlobalConfigChanged();
@@ -100,17 +38,5 @@ public class GlobalConfig
     public event GlobalConfigChanged? OnCurrentNavChanged;
     public event LoadingChanged? OnLoadingChanged;
 
-    #endregion
-
-    #region Method
-
-    public void Initialization(IRequestCookieCollection cookies)
-    {
-        _isDark = Convert.ToBoolean(cookies[IsDarkCookieKey]);
-        _pageMode = cookies[PageModeKey];
-        _navigationMini = Convert.ToBoolean(cookies[NavigationMiniCookieKey]);
-        _expandOnHover = Convert.ToBoolean(cookies[ExpandOnHoverCookieKey]);
-        _favorite = cookies[FavoriteCookieKey];
-    }
     #endregion
 }
