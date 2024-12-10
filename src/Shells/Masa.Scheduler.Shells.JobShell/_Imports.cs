29ff9dfea33b202bab4eede90ec7568cbbcf14fb
wzh425 <wuzihao425@outlook.com>
2024-12-10 10:56:16 +0800

    global using sort

--- src/Shells/Masa.Scheduler.Shells.JobShell/_Imports.cs
+++ src/Shells/Masa.Scheduler.Shells.JobShell/_Imports.cs
@@ -1,12 +1,19 @@
 ﻿// Copyright (c) MASA Stack All rights reserved.
 // Licensed under the Apache License. See LICENSE.txt in the project root for license information.
 
+// System Namespaces
+global using System.Reflection;
+global using System.Text.Json;
+
+// Microsoft Namespaces
+global using Microsoft.AspNetCore.Builder;
+global using Microsoft.Extensions.Configuration;
+global using Microsoft.Extensions.DependencyInjection;
+
+// MASA Building Blocks and Contrib Packages
 global using Masa.BuildingBlocks.StackSdks.Scheduler;
 global using Masa.BuildingBlocks.StackSdks.Scheduler.Model;
 global using Masa.Contrib.StackSdks.Tsc;
+
+// MASA Scheduler Specific
 global using Masa.Scheduler.Shells.JobShell.Models;
-global using Microsoft.AspNetCore.Builder;
-global using Microsoft.Extensions.DependencyInjection;
-global using System.Reflection;
-global using System.Text.Json;
-global using Microsoft.Extensions.Configuration;
--- src/Web/Masa.Scheduler.Web.Admin/_Imports.cs
+++ src/Web/Masa.Scheduler.Web.Admin/_Imports.cs
@@ -1,26 +1,52 @@
 ﻿// Copyright (c) MASA Stack All rights reserved.
 // Licensed under the Apache License. See LICENSE.txt in the project root for license information.
 
+// System Namespaces
+global using System.Reflection;
+global using System.Text;
+
+// Third-party Libraries
 global using FluentValidation;
 global using Humanizer;
 global using Humanizer.Localisation;
 global using Mapster;
+
+// Microsoft Namespaces
+global using Microsoft.AspNetCore.Components;
+global using Microsoft.AspNetCore.Components.Forms;
+global using Microsoft.AspNetCore.Components.Rendering;
+global using Microsoft.AspNetCore.SignalR.Client;
+global using Microsoft.Extensions.DependencyInjection;
+global using Microsoft.JSInterop;
+global using Quartz;
+
+// MASA Blazor and Components
 global using Masa.Blazor;
+global using Masa.Stack.Components;
+global using Masa.Stack.Components.Configs;
+global using Masa.Stack.Components.Extensions;
+global using Masa.Stack.Components.Models;
+
+// MASA Building Blocks
 global using Masa.BuildingBlocks.Authentication.Identity;
 global using Masa.BuildingBlocks.Data.Mapping;
-global using Masa.BuildingBlocks.SearchEngine.AutoComplete;
 global using Masa.BuildingBlocks.StackSdks.Alert;
 global using Masa.BuildingBlocks.StackSdks.Alert.Enum;
 global using Masa.BuildingBlocks.StackSdks.Alert.Model;
 global using Masa.BuildingBlocks.StackSdks.Auth;
 global using Masa.BuildingBlocks.StackSdks.Auth.Contracts;
+global using Masa.BuildingBlocks.StackSdks.Auth.Contracts.Model;
 global using Masa.BuildingBlocks.StackSdks.Config;
 global using Masa.BuildingBlocks.StackSdks.Pm;
 global using Masa.BuildingBlocks.StackSdks.Pm.Model;
 global using Masa.BuildingBlocks.StackSdks.Tsc;
 global using Masa.BuildingBlocks.StackSdks.Tsc.Contracts.Model;
+
+// MASA Contrib Packages
 global using Masa.Contrib.StackSdks.Caller;
 global using Masa.Contrib.StackSdks.Config;
+
+// MASA Scheduler Contracts and Models
 global using Masa.Scheduler.ApiGateways.Caller;
 global using Masa.Scheduler.Contracts.Server.Dtos;
 global using Masa.Scheduler.Contracts.Server.Infrastructure.Consts;
@@ -30,22 +56,9 @@ global using Masa.Scheduler.Contracts.Server.Model;
 global using Masa.Scheduler.Contracts.Server.Requests.SchedulerJobs;
 global using Masa.Scheduler.Contracts.Server.Requests.SchedulerResources;
 global using Masa.Scheduler.Contracts.Server.Requests.SchedulerTasks;
+
+// MASA Scheduler Web Admin Components and Store
 global using Masa.Scheduler.Web.Admin.Components.AlarmRules;
-global using Masa.Scheduler.Web.Admin.Pages.SchedulerResources.Components;
-global using Masa.Stack.Components;
-global using Masa.Stack.Components.Extensions;
-global using Masa.Stack.Components.Models;
-global using Masa.Stack.Components.Configs;
-global using Microsoft.AspNetCore.Components;
-global using Microsoft.AspNetCore.Components.Forms;
-global using Microsoft.AspNetCore.Components.Rendering;
-global using Microsoft.AspNetCore.Http;
-global using Microsoft.AspNetCore.SignalR.Client;
-global using Microsoft.Extensions.DependencyInjection;
-global using Microsoft.JSInterop;
-global using Quartz;
-global using System.Reflection;
-global using System.Text;
 global using Masa.Scheduler.Web.Admin.Components.AlarmRules.ViewModel;
-global using Masa.BuildingBlocks.StackSdks.Auth.Contracts.Model;
+global using Masa.Scheduler.Web.Admin.Pages.SchedulerResources.Components;
 global using Masa.Scheduler.Web.Admin.Store;
